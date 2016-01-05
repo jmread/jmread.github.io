0e1d904bfc36718038a3def1d8584eeee2b8d50f
Jesse Read <jesse.read@gmail.com>
2016-01-05 13:56:06 +0200

    added sources, pages, and build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+ADPATH="$HOME/apps/usr/local/bin"
+ADPATH="/usr/bin/asciidoc"
+ADPATH="$HOME/Downloads/asciidoc-8.6.9/asciidoc.py"
+ASCIIDOC="$ADPATH --backend=xhtml11 \
+	-a linkcss \
+	-a stylesdir=style \
+	-a stylesheet=style.css \
+	-f $PWD/conf/xhtml11.conf \
+	-a max-width=1024px"
+
+
+for INPUT in  *.adoc ; do
+	# If a file ending in ".txt" then process with AsciiDoc.
+	if [ -f $INPUT ] && [ `echo $INPUT | grep -c ".adoc$"` == 1 ] ; then
+		echo "Processing $INPUT"
+		OUTPUT=`basename $INPUT .adoc`
+		$ASCIIDOC --out-file $OUTPUT.html $INPUT;
+
+		# Else if a directory, process its contents.
+	elif [ -d $INPUT ] ; then
+		echo "Processing directory $INPUT"
+		process_asciidoc `ls $INPUT/*`
+	fi
+done
+
